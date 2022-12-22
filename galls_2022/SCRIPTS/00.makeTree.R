196894296b67d9393653b8b8870640888ba9315c
andrew-hipp <andrew.hipp@gmail.com>
2022-12-22 10:14:10 -0600

    updating with new data, cleaning out files

--- galls_2022/SCRIPTS/00.makeTree.R
+++ galls_2022/SCRIPTS/00.makeTree.R
@@ -5,7 +5,7 @@ library(openxlsx)
 source('https://raw.githubusercontent.com/andrew-hipp/morton/master/R/simplePhylo.R')
 
 # set working directory
-setwd('v1/galls_2022/WORKING/')
+setwd('galls_2022/WORKING/')
 
 # set variables:
 now <- format(Sys.time(), "%y%m%d_%Hh%M")
